1d1b17779dd75515c0e58daa407c465c5b871dc8
Alex Harken <aharken@asu.edu>
2018-10-21 10:48:00 -0700

    Updated home screen

--- clientSSR/pages/index.js
+++ clientSSR/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import {connect} from 'react-redux';
 import {setUser} from '../store/actions/auth';
 import {setTest} from '../store/actions/test';
+import Typography from '@material-ui/core/Typography';
 import NavBars from '../components/NavBars/NavBars';
 import Head from 'next/head';
 const homePage = (props)=>(
@@ -11,8 +12,21 @@ const homePage = (props)=>(
     <Head>
       <title>Muscal Goggles | Home</title>
     </Head>
-    <h1>Hello How Are U? {props.isServer? "This is from server":"From another client"}</h1>
-    <Link href = "/blog/asdfasdf"><a>Blog page </a></Link>
+    <body>
+      <div>
+        <Typography variant="h2" color="Primary" align="center" paragraph='true'>
+          Muscal Goggles
+        </Typography> 
+      </div>
+      <div>
+        <Typography variant="h5" color="textSecondary" align="center" paragraph='false'>
+          This is a subheading
+        </Typography>
+        <Typography variant="body1" color="textPrimary" align="center" paragraph='true'>
+          Lorem ipsum this is a paragraph
+        </Typography>
+      </div>
+    </body>
   </div>
 );
 homePage.getInitialProps = async function(context){
