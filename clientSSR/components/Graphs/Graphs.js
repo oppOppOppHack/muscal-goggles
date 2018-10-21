c2ca4746d93c63a71b4b0839eae881714bf685e1
Ricky Hsu <rickyhsu0101@gmail.com>
2018-10-21 13:50:30 -0700

    lkdfjlsadflj

--- clientSSR/components/Graphs/Graphs.js
+++ clientSSR/components/Graphs/Graphs.js
@@ -5,13 +5,13 @@ const data01 = [{x: 100, y: 30}, {x: 325, y: 200}, {x:255, y: 100}, {x: 150, y: 
 const data02 = [{x: 130, y: 20}, {x: 250, y: 180}, {x: 275, y: 240}, {x: 200, y: 100}, {x: 320, y: 190}];
 
 const data = [
-  {name: 'A - D', uv: 0, amt: 2400},
-  {name: 'E - H', uv: 0, amt: 2210},
-  {name: 'H - K', uv: 1175, amt: 2290},
-  {name: 'K - N', uv: 0, amt: 2000},
-  {name: 'O - R', uv: 1820, amt: 2181},
-  {name: 'S - V', uv: 0, amt: 2500},
-  {name: 'W - Z', uv: 0, amt: 2100},
+  {name: 'A - D', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'E - H', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'H - K', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'K - N', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'O - R', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'S - V', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'W - Z', uv: 3490, pv: 4300, amt: 2100},
 ];
 
 /* My name is Ricky, as far as you know. */
@@ -38,6 +38,9 @@ class Graphs extends Component
           <XAxis dataKey="name"/>
           <YAxis/>
           <Tooltip/>
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
         </BarChart>
       </div>
     );
