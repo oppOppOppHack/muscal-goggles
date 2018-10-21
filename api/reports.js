e24e86a588b8e46d6ef9dcc7c6dfef4debef0f1d
Tyman Sin <sin.tyman@yahoo.com>
2018-10-21 11:08:06 -0700

    Added ReportTemplate endpoints

--- api/reports.js
+++ api/reports.js
@@ -1,105 +1,110 @@
 const router = require("express").Router();
-const passport = require("passport");
-const mongoose = require("mongoose");
-const async = require("async");
+const mongoose = require('mongoose');
 
-const Metric = mongoose.model("metrics");
-const Template = mongoose.model("templates");
-const Event = mongoose.model("events");
-const Object = mongoose.model("objects");
-// @route   GET api/reports/generate/:eventTemplatename
-// @desc    Generate metrics for the event
+const ReportTemplate = mongoose.model('reportTemplates');
+
+// @route   POST api/reports/addTemplate
+// @desc    add a report template
 // @access  Private
-router.get("/generate/:eventTemplateName", passport.authenticate('jwt',{session: false}), (req,res)=>{
-  Template.findOne({name: req.params.eventTemplateName})
+router.post('/addTemplate/:name', passport.authenticate('jwt',{session: false}),(req, res) => {
+	const {metrics} = req.body;
+	const rtemplate = {
+		metrics
+	};
+	const rtemplateInstance = new ReportTemplate(rtemplate);
+	rtemplateInstance.save()
+		.then(template=>{
+			return res.json({
+				success: true,
+				template
+			});
+
+		})
+		.catch(err=>{
+			return res.status(400).json({
+				success: false,
+				msg: "Something went wrong"
+			})
+		})
+});
+
+// @route   GET api/templates/get/:id
+// @desc    Get specific report Template
+// @access  Private
+router.get("/get/:id", passport.authenticate('jwt', {session: false}),(req, res)=>{
+  ReportTemplate.findById(req.params.id)
     .then(template=>{
-      if(!template)
+      if(!template){
         return res.status(404).json({
           success: false,
-          msg: "Not found"
+          msg: "Not Found"
+        })
+      }
+      if(template.organization == req.user.organization){
+        return res.json({
+          success: true,
+          template
         });
-      Metric.deleteMany({template: template.id})
-        .then(err=>{
-        Event.find({templateId: template.id})
-          .then(events=>{
-            //get the non-object fields and manipulate
-            
-            let regularFields = template.fields.map(el=>{
-              if(el.type =="object"){
-                return null
-              }else{
-                return el.name;
-              }
-            }).filter(el=>{
-              return el!=null
-            });
-            let metricsData = [];
-            regularFields.forEach(field=>{
-              let metric = {field, template: template.id, templateType: 'event', dataPoints:[]};
-              events.forEach(event=>{
-                metric.dataPoints.push({
-                  event: event.id,
-                  data: event.data[field]
-                });
-              });
-              metricsData.push(metric);
-            })
-            console.log(metricsData);
-            let objectFunct = template.fields.filter(el=>{
-              return el.type === 'object'
-            }).map(el=>{
-              //console.log(el);
-              return callback=>{
-                Template.findOne({name: el.name})
-                  .then(objTemplate=>{
-                    //console.log(objTemplate);
-                    Object.find({templateId: objTemplate.id})
-                    .then(objects=>{
-                      //console.log(objects);
-                      let metricsForObjects = [];
-                      //console.log(objects);
-                      //console.log(objects);
-                      objects.forEach(obj=>{
-                        
-                        let eventSpecific = events.find(ele=>{
-                          //console.log(ele);
-                          //console.log(obj);
-                          return ele.data[el.name]===obj.data.Name}
-                        );
-                        if(eventSpecific){
-                          metricsForObjects.push({
-                            event: eventSpecific.id,
-                            data: obj.data
-                          });
-                        }
-                      });
-                     // console.log(metricsForObjects);
-                      metricsData.push({
-                        field: el.name,
-                        template: template.id,
-                        templateType: "event",
-                        type: "object",
-                        dataPoints: metricsForObjects
-                      });
-                      console.log(metricsData);
-                      callback(null, true);
-                    })
-                  })
-              }
-            });
-            async.series(objectFunct, (err, result)=>{
-              console.log(metricsData);
-              console.log(metricsData[0].dataPoints);
-              Metric.insertMany(metricsData, (err, docs) => {
-                res.json({
-                  success: true,
-                })
-              })
-            })
-          })
-      })
-    });
+      }else{
+        return res.status(401).json({
+          success: false,
+          msg: "Something went wrong"
+        })
+      }
+    })
+		.catch(err => {
+			return res.status(500).json({
+				success: false,
+				msg: "Database access error"
+			});
+		});
 });
-//
+
+// @route   POST api/reports/getTemplates
+// @desc    get all report templates
+// @access  Private
+router.post('/getTemplates', passport.authenticate('jwt',{session: false}),(req, res) => {
+	ReportTemplate.find({})
+		.then(templates => {
+			if(!template){
+				return res.status(404).json({
+					success: false,
+					msg: "Could not find report templates"
+				});
+			}
+			else{
+				return res.json({
+					success: true,
+					msg: "Got all report templates",
+					templates
+				});
+			}
+		})
+		.catch(err => {
+			return res.status(500).json({
+				success: false,
+				msg: "Database access error"
+			});
+		});
+});
+
+// @route   POST api/reports/removeTemplate
+// @desc    remove a report template
+// @access  Private
+router.post('/removeTemplate/:name', passport.authenticate('jwt',{session: false}),(req, res) => {
+	ReportTemplate.findById(req.params.id)
+		.then(template=>{
+			if(!template)
+				return res.status(404).json({
+					success: false,
+					msg: "Not Found"
+				})
+			ReportTemplate.findById(req.params.id).remove(()=>{
+					return res.json({
+						success: true
+					})
+				})
+		})
+});
 
 module.exports= router;
