5428116b5bdea67f98daecd3a74b0024c9f38104
jzhu379 <jzhu379@gmail.com>
2018-10-21 01:18:36 -0400

    nav bars

---
+++ clientSSR/components/NavBars/NavBars.js
@@ -0,0 +1,91 @@
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import React, {Component} from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const styles =
+{
+  root: {
+  flexGrow: 1,
+  },
+  grow: {
+  flexGrow: 1,
+  },
+  list: {position: 'relative', width: '15vw'}
+};
+
+class NavBars extends Component
+{
+  state = {showing: false}
+  
+  render()
+  {
+    const {classes} = this.props;
+
+    return (
+    <div className = {classes.root}>
+      <SwipeableDrawer
+        open = {this.state.showing}
+        onClose = {() => {
+          this.setState({showing: false});
+        }}
+        onOpen = {() => {
+          this.setState({showing: true});
+        }}
+      >
+        <div
+          tabIndex = {0}
+          role = "button"
+          className = {classes.list}
+          onClick = {() => {
+            this.setState({showing: false});
+          }}
+          onKeyDown = {() => {
+            this.setState({showing: true});
+          }}
+        >
+          <ListItem button>
+            <ListItemText primary = "Option 1" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary = "Option 2" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary = "Option 3" />
+          </ListItem>
+        </div>
+      </SwipeableDrawer>
+      <AppBar position = "static">
+        <Toolbar>
+          <IconButton
+            className = {classes.menuButton}
+            color = "inherit"
+            aria-label = "Menu"
+            onClick = {() =>
+            {
+              this.setState({showing: true});
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            TITLE
+          </Typography>
+          <Button color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+    );
+  }
+}
+
+export default withStyles(styles)(NavBars);
