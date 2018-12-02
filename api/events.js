1cb2e244b1c9a15c098e5cb9a1698d78ab69dc02
jzhu379 <jzhu379@gmail.com>
2018-12-01 22:55:25 -0500

    some edits

--- api/events.js
+++ api/events.js
@@ -10,80 +10,81 @@ const Object = mongoose.model('objects');
 // @desc    add a list of events
 // @access  Private
 router.post('/add/:name', passport.authenticate('jwt',{session: false}),(req, res) => {
-	// expects array of events with the same templateId to be inserted
-	// validate input
-	if(!Array.isArray(req.body) || req.body.length < 1)
-	{
-		return res.status(400).json({
-			success: false,
-			msg: "Bad Request Body"
-		});
-	}
-	Template.findOne({name: req.params.name})
-		.then(template=>{
-			if(!template)
-				return res.status(404).json({
-					success: false,
-					msg: "Template not found"
-				});
+  // expects array of events with the same templateId to be inserted
+  // validate input
+  
+  if(!Array.isArray(req.body) || req.body.length < 1)
+  {
+    return res.status(400).json({
+      success: false,
+      msg: "Bad Request Body"
+    });
+  }
+  Template.findOne({name: req.params.name})
+    .then(template=>{
+      if(!template)
+        return res.status(404).json({
+          success: false,
+          msg: "Template not found"
+        });
 
-			const eventDocs = req.body.map(el=>{
-				return {
-					data: el,
-					templateId: template._id
-				}
-			})
+      const eventDocs = req.body.map(el=>{
+        return {
+          data: el,
+          templateId: template._id
+        }
+      })
 
-			Event.collection.insert(eventDocs, (err, docs) => {
-				// on error
-				if (err) {
-					return res.status(500).json({
-						success: false,
-						msg: "Error adding events"
-					});
-				} else {
-					return res.status(200).json({
-						success: true,
-						msg: "Added events",
-						eventArray: docs
+      Event.collection.insert(eventDocs, (err, docs) => {
+        // on error
+        if (err) {
+          return res.status(500).json({
+            success: false,
+            msg: "Error adding events"
+          });
+        } else {
+          return res.status(200).json({
+            success: true,
+            msg: "Added events",
+            eventArray: docs
 
-					});
-				}
-			})
-		});
+          });
+        }
+      })
+    });
 });
 //TODO:
 // @route   POST api/events/remove
 // @desc    remove events by uid
 // @access  Private
 router.post('/remove', (req, res) => {
-	// expects an array of Event uids to remove
-	// validate input
-	if(typeof(req.body) != array || req.body.length < 1) {
-		return res.send(400).json({
-			success: false,
-			msg: "Bad Request Body"
-		});
-	}
+  // expects an array of Event uids to remove
+  // validate input
+  if(typeof(req.body) != array || req.body.length < 1) {
+    return res.send(400).json({
+      success: false,
+      msg: "Bad Request Body"
+    });
+  }
 
-	uidList = req.body;
+  uidList = req.body;
 
-	//TODO: Probably more verification
+  //TODO: Probably more verification
 
-	Event.deleteMany({_id: { $in: uidList }}, (err) => {
-		if(err) {
-			return res.send(500).json({
-				success: false,
-				msg: "Failed to delete events from database"
-			});
-		}
-		else {
-			return res.send(200).json({
-				success: true,
-				msg: "Deleted events from database"
-			});
-		}
-	});
+  Event.deleteMany({_id: { $in: uidList }}, (err) => {
+    if(err) {
+      return res.send(500).json({
+        success: false,
+        msg: "Failed to delete events from database"
+      });
+    }
+    else {
+      return res.send(200).json({
+        success: true,
+        msg: "Deleted events from database"
+      });
+    }
+  });
 });
 
 // @route   POST api/events/getEventsByUID
@@ -91,25 +92,25 @@ router.post('/remove', (req, res) => {
 // @access  Private
 router.post('/getEventsByParams/:templateId', (req, res) => {
 /*
-	{
-		<field>: <value>
-	}
+  {
+    <field>: <value>
+  }
 */
 
-			Event.find({data: req.body, tempateId:req.params.templateId}, (err, docs) => {
-				if (err) {
-					return res.send(500).json({
-						success: false,
-						msg: "Could not find events"
-					});
-				} else {
-					return res.send(200).json({
-						success: true,
-						msg: "Found events",
-						data: docs
-					});
-				}
-			});
+      Event.find({data: req.body, tempateId:req.params.templateId}, (err, docs) => {
+        if (err) {
+          return res.send(500).json({
+            success: false,
+            msg: "Could not find events"
+          });
+        } else {
+          return res.send(200).json({
+            success: true,
+            msg: "Found events",
+            data: docs
+          });
+        }
+      });
 
 
 });
@@ -118,23 +119,23 @@ router.post('/getEventsByParams/:templateId', (req, res) => {
 // @desc    return a list of events with the given templateId(s)
 // @access  Private
 router.post('/getEventsByTemplateID', (req, res) => {
-	// expects array of templateIds to return results for
-	// validate input
-	tidList = req.body;
-	Event.find({templateId: { $in: tidList }}, (err, docs) => {
-		if(err) {
-			return res.send(500).json({
-				success: false,
-				msg: "Could not find events"
-			});
-		}
-		else {
-			return res.send(200).json({
-				success: true,
-				msg: "Found events",
-				data: docs
-			});
-		}
-	});
+  // expects array of templateIds to return results for
+  // validate input
+  tidList = req.body;
+  Event.find({templateId: { $in: tidList }}, (err, docs) => {
+    if(err) {
+      return res.send(500).json({
+        success: false,
+        msg: "Could not find events"
+      });
+    }
+    else {
+      return res.send(200).json({
+        success: true,
+        msg: "Found events",
+        data: docs
+      });
+    }
+  });
 });
 module.exports = router;
